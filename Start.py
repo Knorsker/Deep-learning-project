87a4b6909d1c57b4c07152ffa73518f08550d0f1
KatjaNorsker <katja@norsker.com>
2023-10-30 15:09:49 +0100

    Fjernet CUDA

--- Start.py
+++ Start.py
@@ -5,7 +5,6 @@ from audiotools import AudioSignal
 model_path = dac.utils.download(model_type="44khz")
 model = dac.DAC.load(model_path)
 
-model.to('cuda')
 
 # Load audio signal file
 signal = AudioSignal('input.wav')
