1c35a2e6960248cc611bc09b5d83467c97ac4dc1
jkboel <juliekofodboel@gmail.com>
2023-10-30 14:57:30 +0100

    Mere

--- Start.py
+++ Start.py
@@ -10,7 +10,28 @@ model.to('cuda')
 # Load audio signal file
 signal = AudioSignal('input.wav')
 
+# Encode audio signal as one long file
+# (may run out of GPU memory on long files)
+signal.to(model.device)
 
+x = model.preprocess(signal.audio_data, signal.sample_rate)
+z, codes, latents, _, _ = model.encode(x)
 
+# Decode audio signal
+y = model.decode(z)
 
+# Alternatively, use the `compress` and `decompress` functions
+# to compress long files.
 
+signal = signal.cpu()
+x = model.compress(signal)
+
+# Save and load to and from disk
+x.save("compressed.dac")
+x = dac.DACFile.load("compressed.dac")
+
+# Decompress it back to an AudioSignal
+y = model.decompress(x)
+
+# Write to file
+y.write('output.wav')
