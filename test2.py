e33bfffd765c4492100460bca7d69e24e7ef9d2f
jkboel <juliekofodboel@gmail.com>
2023-11-13 13:15:52 +0100

    -

--- test2.py
+++ test2.py
@@ -11,19 +11,19 @@ print(a**2)
 print(v+b)
 
 
-# import dac
-# from audiotools import AudioSignal
+import dac
+from audiotools import AudioSignal
 
-# Download a model
-# model_path = dac.utils.download(model_type="44khz")
-# model = dac.DAC.load(model_path)
+#Download a model
+model_path = dac.utils.download(model_type="44khz")
+model = dac.DAC.load(model_path)
 
-# model.to('cuda')
+model.to('cuda')
 
-# Load audio signal file
-# file = '/work3/s164396/data/DNS-Challenge-4/datasets_fullband/clean_fullband/vctk_wav48_silence_trimmed/p225/p225_001_mic1.wav'
-# signal = AudioSignal(file)
-# print(signal)
+#Load audio signal file
+file = '/work3/s164396/data/DNS-Challenge-4/datasets_fullband/clean_fullband/vctk_wav48_silence_trimmed/p225/p225_001_mic1.wav'
+signal = AudioSignal(file)
+print(signal)
 
 
 
