27b2df3c9abd744025d758759ab92c3fcfeb484c
jkboel <juliekofodboel@gmail.com>
2023-11-17 14:54:15 +0100

    -

--- simple.sh
+++ simple.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#SBATCH --job-name=test_ejk
+#SBATCH --partition=gpuv100
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=00:30:00
+
+
 # Activate the virtual environment
 source /zhome/87/1/168411/dl-venv/bin/activate
 
