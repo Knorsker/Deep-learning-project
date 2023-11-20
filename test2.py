ac4dc37005465d95a12cfa1a1f0c7e8fcb885757
jkboel <juliekofodboel@gmail.com>
2023-11-20 14:14:01 +0100

    -

--- test2.py
+++ test2.py
@@ -11,7 +11,6 @@ print(a**2)
 print(v+b)
 
 
-import dac
 from help import download, load_model
 import torchaudio
 
@@ -19,30 +18,7 @@ import torchaudio
 file = '/work3/s164396/data/DNS-Challenge-4/datasets_fullband/clean_fullband/vctk_wav48_silence_trimmed/p225/p225_001_mic1.wav'
 print('file loaded')
 signal = torchaudio.load(file)
-print(signal)
+print('done')
 
-# import sys
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 2:
-#         print("Usage: test2.py p225_001_mic1.wav")
-#         sys.exit(1)
-
-#     input_data_file = sys.argv[1]
-
-#     # Now you can use 'input_data_file' in your script to access the data
-#     try:
-#         with open(input_data_file, 'r') as file:
-#             # Process the data or perform other operations
-#             data = file.read()
-#             print(f"Successfully read data from {input_data_file}:\n{data}")
-#     except FileNotFoundError:
-#         print(f"Error: File not found - {input_data_file}")
-#         sys.exit(1)
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-#         sys.exit(1)
-
-# print('yes')
 
 
