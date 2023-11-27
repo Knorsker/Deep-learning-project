ad12a73be83d59a33b6b3e25bbfd48df04005508
jkboel <juliekofodboel@gmail.com>
2023-11-27 08:03:56 +0100

    -

--- run_HUB.sh
+++ run_HUB.sh
@@ -3,11 +3,11 @@
 ### General options
 ### -- specify queue -
 #BSUB -q gpuv100
-#BSUB -gpu "num=4"
+#BSUB -gpu "num=3"
 ### -- set the job Name -
 #BSUB -J HUBER_Julie_Boel_s214704
 ### -- ask for number of cores (default: 1) - 4 cores per gpu
-#BSUB -n 16
+#BSUB -n 12
 ### -- specify that the cores must be on the same host -
 #BSUB -R "span[hosts=1]" 
 ### -- specify that we need x GB of memory per core/slot -
