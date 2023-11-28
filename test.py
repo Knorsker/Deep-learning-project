1aa2b5fece8dcdb25b91bda4a54f507d111cfa80
jkboel <juliekofodboel@gmail.com>
2023-11-28 20:07:11 +0100

    -

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-# from torch.cuda.random import set_rng_state
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
 import os
 import torch
 import numpy as np
-from audiotools.audiotools.core import AudioSignal
+from sklearn.model_selection import train_test_split
 
+torch.manual_seed(42)
 
 class AudioData:
     def __init__(self, audio_data, sample_rate):
@@ -21,7 +21,7 @@ class AudioData:
         return self.audio_data.device
 
     def clone(self):
-        return self.audio_data.clone()
+        return AudioData(self.audio_data.clone(), self.sample_rate)
 
     def audio_data(self):
         return self.audio_data
@@ -55,22 +55,22 @@ class AudioDataset(Dataset):
 
         # Convert to mono if stereo
         if signal.audio_data.shape[0] > 1:
-            signal = signal.to_mono()
+            signal.audio_data = torch.mean(signal.audio_data, dim = -2, keepdim = True)
         if noise.audio_data.shape[0] > 1:
-            noise = noise.to_mono()
+            noise.audio_data = torch.mean(noise.audio_data, dim = -2, keepdim = True)
 
         # Pad or trim to the specified length of 5 seconds
-        self.fixed_length = int(signal.sample_rate*2)
+        self.fixed_length = int(signal.sample_rate*1)
         current_length = signal.audio_data.shape[1]
         if current_length < self.fixed_length:
-                # Pad if the signal is shorter than the fixed length
+            # Pad if the signal is shorter than the fixed length
             padding = self.fixed_length - current_length
             signal.audio_data = torch.nn.functional.pad(signal.audio_data, (0, padding))
         elif current_length > self.fixed_length:
             start_position = np.random.randint(0, max(1, signal.audio_data.shape[1] - self.fixed_length))
             signal.audio_data = signal.audio_data[:, start_position:start_position + self.fixed_length]
 
-        self.fixed_length = int(noise.sample_rate*5) 
+        self.fixed_length = int(noise.sample_rate*1) 
         current_length = noise.audio_data.shape[1]
         if current_length < self.fixed_length:
             padding = self.fixed_length - current_length
@@ -82,7 +82,11 @@ class AudioDataset(Dataset):
         return signal, noise
 
 def collate_fn(batch):
-    return batch
+    speech, noise = zip(*batch)
+    speech = [signal.audio_data for signal in speech]
+    noise = [signal.audio_data for signal in noise]
+
+    return torch.stack(speech), torch.stack(noise)
 
 # Specify the file paths as before
 # drive_path = r'C:\Users\julie\OneDrive\Skrivebord\Deep Learning, 02456\Project\Deep-learning-project'
@@ -94,10 +98,21 @@ noise_folder = "noise_fullband"
 file_paths_sound = [os.path.join(drive_path, audio_folder, filename) for filename in os.listdir(os.path.join(drive_path, audio_folder))]
 file_paths_noise = [os.path.join(drive_path, noise_folder, filename) for filename in os.listdir(os.path.join(drive_path, noise_folder))]
 
-# Create dataset and dataloader
-audio_dataset = AudioDataset(file_paths_sound, file_paths_noise)
-batch_size = 32
-dataloader = DataLoader(audio_dataset, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=collate_fn)
+# Split the data into training and test sets
+train_paths_sound, test_paths_sound = train_test_split(
+    file_paths_sound, test_size=0.2, random_state=42
+)
+train_paths_noise, test_paths_noise = train_test_split(
+    file_paths_noise, test_size=0.2, random_state=42
+)
+
+# Create datasets and dataloaders for training and testing
+train_dataset = AudioDataset(train_paths_sound, train_paths_noise)
+test_dataset = AudioDataset(test_paths_sound, test_paths_noise)
+
+batch_size = 16
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2, collate_fn=collate_fn)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2, collate_fn=collate_fn)
 
 print('Dataloader - done')
 
@@ -105,12 +120,12 @@ print('Dataloader - done')
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import dac
 import torchaudio.functional as F
 import torchaudio
-from help import download, DAC
+from help import download, DAC, add_noise
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-num_epochs = 10
+num_epochs = 100 #First: 50
 
 # Create the model, loss function, and optimizer
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -120,82 +135,126 @@ target_sample_rate = 44100  # Replace with your desired sample rate
 # Download a model
 model_path = download(model_type="44khz")
 model = DAC.load(model_path)
-model = model.to(device)
-criterion = nn.MSELoss() # NOTE !!! HUBERLOSS, L1-loss, 
-lr = 1e-4
+
+model = model.to(device).train()
+criterion = nn.MSELoss() # NOTE 
+lr = 1e-5 #First: 1e-4
 optimizer = optim.AdamW(model.parameters(), lr=lr)
 
-loss_vec = []
+scheduler = ReduceLROnPlateau(optimizer, mode='min', patience=5, factor=0.1)
+
+train_loss_vec = []
+test_loss_vec = []
 epoch_vec = []
-output = []
+snr = torch.tensor([[0]]).cuda()
 
-# Training loop
-for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0.0
+# # Training loop
+# for epoch in range(num_epochs):
+#     model.train()
+#     total_loss = 0.0
 
-    for signals_batch in dataloader:
-        # Iterate over signals within the batch
-        for signal, noise in signals_batch:
-            # Calculate signal and noise power to find SNR value
-            signal_power = np.sum(signal.audio_data.numpy()**2) / len(signal.audio_data.numpy())
-            noise_power = np.sum(noise.audio_data.numpy()**2) / len(noise.audio_data.numpy())
+#     for signal, noise in train_dataloader:
+#         signal = signal.cuda()
+#         noise = noise.cuda()
 
-            # If noise_power or L2-norm of noise is 0, correct it
-            if noise_power == 0 or torch.norm(noise.audio_data, p = 2) == 0:
-                noise.audio_data += 1e-5
-                noise_power += 1e-5
-            # Calculate signal to noise ratio
-            snr_db = int(10 * np.log10(signal_power / noise_power))
-            # Define noisy signal
-            noisy_signal = signal
-            # Create noisy signal
-            noise_constant = torch.sqrt((torch.norm(signal.audio_data,p=2)**2/torch.norm(noise.audio_data,p=2)**2)*10**(-snr_db/10))
-            noisy_signal.audio_data = signal.audio_data + noise_constant*noise.audio_data
-            # Zero the gradients
-            optimizer.zero_grad()
+#         # Create noisy signal
+#         noise += 1e-5
+#         noisy_signal = add_noise(signal, noise, snr)
 
-            # Forward pass
-            noisy_signal.audio_data.requires_grad_(True)
-            noisy_signal.to(model.device)
-            noisy_signal.audio_data = torch.unsqueeze(noisy_signal.audio_data,dim=1)
-            x = model.preprocess(noisy_signal.audio_data, noisy_signal.sample_rate.item())
-            z, codes, latents, _, _ = model.encode(x)
+#         """
+#         # Normalize Signals
+#         signal_audio_data = signal.audio_data/torch.max(torch.abs(signal.audio_data))
+#         noisy_signal.audio_data = noisy_signal.audio_data/torch.max(torch.abs(noisy_signal.audio_data))
+#         """
+#         # Zero the gradients
+#         optimizer.zero_grad()
 
-            # Decode audio signal
-            y = model.decode(z)
+#         # Forward pass
+#         noisy_signal.to(model.device)
 
-            if y.shape[2] > signal.audio_data.shape[2]:
-                y = y[:, :, :signal.audio_data.shape[2]]
-            elif y.shape[2] < signal.audio_data.shape[2]:
-                padding = signal.audio_data.shape[2] - y.shape[2]
-                y = torch.nn.functional.pad(y, (0, padding))
+#         x = model.preprocess(noisy_signal, 44100)
+#         z, codes, latents, _, _ = model.encode(x)
 
-            # Calculate the loss for the current signal
-            loss = criterion(y, signal.audio_data)
+#         # Decode audio signal
+#         y = model.decode(z)
 
-            # Backpropagation
-            loss.backward()
+#         if y.shape[2] > signal.shape[2]:
+#             y = y[:, :, :signal.shape[2]]
+#         elif y.shape[2] < signal.shape[2]:
+#             padding = signal.shape[2] - y.shape[2]
+#             y = torch.nn.functional.pad(y, (0, padding))
 
-            # Update model parameters
-            optimizer.step()
+#         # Calculate the loss for the current signal
+#         loss = criterion(y, signal)
 
-            total_loss += loss.item()
-        loss_vec.append(total_loss / len(dataloader))
-        epoch_vec.append(epoch)
+#         # Backpropagation
+#         loss.backward()
 
-    # Print training statistics
+#         # Update model parameters
+#         optimizer.step()
 
-    # print("------------------------------------------------------------------------")
-    # print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {total_loss / len(dataloader)}")
-    # print("------------------------------------------------------------------------")
+#         total_loss += loss.item()
 
-    output.append(total_loss / len(dataloader))
+#     # Print training statistics
+#     train_loss_vec.append(total_loss / len(train_dataloader))
+#     epoch_vec.append(epoch)
+#     # print("------------------------------------------------------------------------")
+#     # print(f'Epoch [{epoch + 1}/{num_epochs}] - Loss: {total_loss / len(train_dataloader)}')
+#     # print("------------------------------------------------------------------------")
+
+
+#     # Testing
+#     model.eval()
+#     total_test_loss = 0.0
+
+#     with torch.no_grad():
+#         for signals_batch in test_dataloader:
+#             signal = signal.cuda()
+#             noise = noise.cuda()
+#             # Create noisy signal
+#             noise += 1e-5
+#             noisy_signal = add_noise(signal, noise, snr)
+
+#             """
+#             # Normalize Signals
+#             signal_audio_data = signal.audio_data/torch.max(torch.abs(signal.audio_data))
+#             noisy_signal.audio_data = noisy_signal.audio_data/torch.max(torch.abs(noisy_signal.audio_data))
+#             """
+#             # Zero the gradients
+#             optimizer.zero_grad()
+
+#             # Forward pass
+#             noisy_signal = noisy_signal.to(model.device)
+#             x = model.preprocess(noisy_signal, 44100)
+#             z, codes, latents, _, _ = model.encode(x)
+
+#             # Decode audio signal
+#             y = model.decode(z)
+
+#             if y.shape[2] > signal.shape[2]:
+#                 y = y[:, :, :signal.shape[2]]
+#             elif y.shape[2] < signal.shape[2]:
+#                 padding = signal.shape[2] - y.shape[2]
+#                 y = torch.nn.functional.pad(y, (0, padding))
+
+#             # Calculate the loss for the current signal
+#             loss = criterion(y, signal)
+
+#             total_test_loss += loss.item()
+
+#     # Calculate average test loss
+#     average_test_loss = total_test_loss / len(test_dataloader)
+#     test_loss_vec.append(average_test_loss)
     
-    # print(f'Epoch [{epoch + 1}/{num_epochs}]')
+#     # Print testing statistics
+#     # print(f'Epoch [{epoch + 1}/{num_epochs}] - Test Loss: {average_test_loss}')
+#     # print("------------------------------------------------------------------------")
+    
 
-# np.savetxt('Output', output)    
-# Save the trained model
-# torch.save(model.state_dict(), 'trained_dac_model.pth')
+# np.savetxt('Output_train_MSE', train_loss_vec)
+# np.savetxt('Output_test_MSE', test_loss_vec)    
+
+# # Save the trained model
+# torch.save(model.state_dict(), 'trained_MSE_model.pth')
 
 print('Done')
